9ffc433ab157a9c24b16cdafd52d5a093fb859db
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-16 22:52:46 +0100

    add average_rating method to farm

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -7,5 +7,10 @@ class Farm < ActiveRecord::Base
   validates :zip_code, presence: true
   validates :city, presence: true
   validates :country, presence: true
+
+  def average_rating
+    return 0 if reviews.empty?
+    (reviews.map(&:rating).sum / reviews.count.to_f).round(1)
+  end
 end
 
--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -8,5 +8,18 @@ describe Farm, type: :model do
   it { is_expected.to validate_presence_of(:zip_code) }
   it { is_expected.to validate_presence_of(:city) }
   it { is_expected.to validate_presence_of(:country) }
+
+  describe '#average_rating' do
+    it 'returns 0 if a farm has no ratings' do
+      farm = build(:farm)
+      expect(farm.average_rating).to eq(0)
+    end
+
+    it 'returns the correct average up to 1 decimal place' do
+      farm = build(:farm)
+      [2, 2, 3].each { |rating| create(:review, rating: rating, farm: farm) }
+      expect(farm.average_rating).to eq(2.3)
+    end
+  end
 end
 
