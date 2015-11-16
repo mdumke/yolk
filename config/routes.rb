d224885ba9b0a084af6ccd3da5f7833752dc3484
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-16 22:50:06 +0100

    setup some routes to get started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'application#welcome'
 
+  get 'home', controller: 'farms', action: 'index'
+  get 'sign_up', controller: 'users', action: 'sign_up'
+  get 'sign_in', controller: 'sessions', action: 'sign_in'
+
+  resources :farms, only: [:new, :create, :show]
+  resources :users, only: [:show, :create]
+  resources :reviews, only: [:index, :create]
+
   get 'ui(/:action)', controller: 'ui'
 end
 
