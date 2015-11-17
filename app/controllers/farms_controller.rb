54664cc8169c4e0dd57abb100be26b94d08a02ef
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-17 22:02:18 +0100

    implement ability to add farms

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -5,5 +5,28 @@ class FarmsController < ApplicationController
     @farms = Farm.all
     @review = Review.featured
   end
+
+  def new
+    @farm = Farm.new
+  end
+
+  def create
+    @farm = Farm.new(farm_params)
+
+    if @farm.save
+      flash[:notice] = "#{@farm.name} was added!"
+      redirect_to @farm
+    else
+      flash[:error] = 'The data was not sufficient to create the farm'
+      render 'new'
+    end
+  end
+
+  private
+
+  def farm_params
+    params.require(:farm).permit(
+      :name, :street, :zip_code, :city, :country)
+  end
 end
 
--- spec/controllers/farms_controller_spec.rb
+++ spec/controllers/farms_controller_spec.rb
@@ -22,5 +22,63 @@ describe FarmsController do
       let(:action) { get :index }
     end
   end
+
+  describe 'GET new' do
+    before { login }
+
+    it 'sets a new @farm variable' do
+      get :new
+      expect(assigns(:farm)).to be_a_new_record
+    end
+
+    it_behaves_like 'requires authenticated user' do
+      let(:action) { get :new }
+    end
+  end
+
+  describe 'Post create' do
+    before { login }
+
+    context 'with valid data' do
+      it 'redirects to the new farms show-page' do
+        post :create, farm: attributes_for(:farm)
+        expect(response).to redirect_to farm_path(Farm.last)
+      end
+
+      it 'creates a new farm' do
+        post :create, farm: attributes_for(:farm)
+        expect(Farm.count).to eq 1
+      end
+
+      it 'sets the flash-notice' do
+        post :create, farm: attributes_for(:farm)
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context 'with invalid data' do
+      before { post :create, farm: {invalid: 'data'} }
+
+      it 'does not create a new farm' do
+        expect(Farm.count).to eq 0
+      end
+
+      it 'renders the new-form again' do
+        expect(response).to render_template 'new'
+      end
+
+      it 'sets the @farm variable' do
+        expect(assigns(:farm)).to be_present
+      end
+
+      it 'sets the error-flash' do
+        expect(flash[:error]).to be_present
+      end
+    end
+
+    it_behaves_like 'requires authenticated user' do
+      let(:action) { get :new }
+    end
+  end
 end
 
