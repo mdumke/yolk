61d13f1ce29f6c2f6fc0740171b3a123f8cc1b9d
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-17 19:50:23 +0100

    implement sign-in and sign-out process

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'application#welcome'
 
-  get 'home', controller: 'farms', action: 'index'
-  get 'sign_up', controller: 'users', action: 'new'
-  post 'sign_up', controller: 'users', action: 'create'
-  post 'sign_in', controller: 'sessions', action: 'create'
-  get 'sign_out', controller: 'sessions', action: 'destroy'
+  get  'home',     controller: 'farms', action: 'index'
+  get  'sign_up',  controller: 'users', action: 'new'
+  post 'sign_up',  controller: 'users', action: 'create'
+  get  'sign_in',  controller: 'sessions', action: 'new'
+  post 'sign_in',  controller: 'sessions', action: 'create'
+  get  'sign_out', controller: 'sessions', action: 'destroy'
 
   resources :farms, only: [:new, :create, :show]
   resources :users, only: [:show, :create]
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe SessionsController do
+  describe 'POST create' do
+    let!(:user) { create(:user) }
+
+    context 'with valid data' do
+      before do
+        post :create, email: user.email, password: user.password
+      end
+
+      it 'logs the user in' do
+        expect(session[:user_id]).to eq user.id
+      end
+
+      it 'redirects to the home path' do
+        expect(response).to redirect_to home_path
+      end
+
+      it 'sets the flash-notice' do
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context 'with invalid data' do
+      before { post :create, email: user.email, password: '123_false' }
+
+      it 'does not log the user in' do
+        expect(session[:user_id]).to be_nil
+      end
+
+      it 'render the new template again' do
+        expect(response).to render_template 'new'
+      end
+
+      it 'sets the error-flash' do
+        expect(flash[:error]).to be_present
+      end
+    end
+  end
+
+  describe 'GET destroy' do
+    before do
+      login
+      get :destroy
+    end
+
+    it 'logs the user out' do
+      expect(session[:user_id]).to be_nil
+    end
+
+    it 'redirects to root-path after logout' do
+      expect(response).to redirect_to root_path
+    end
+  end
+end
+
