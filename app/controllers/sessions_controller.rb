7c788f141f7e17698be313dea9d3e8fbfb85fed4
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-23 14:11:56 +0100

    some minor cleanup in the views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome back, #{user.first_name}!"
+      flash[:notice] = "Welcome back, #{user.full_name}!"
       redirect_to home_path
     else
       flash[:error] = "Something is wrong with your email or password"
